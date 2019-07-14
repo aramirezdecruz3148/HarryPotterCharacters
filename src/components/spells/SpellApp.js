4479afccc12200965369960f4530dec49b94d3b5
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-14 16:49:23 -0700

    more renaming and adding of new files

---
+++ src/components/spells/SpellApp.js
@@ -0,0 +1,13 @@
+import Component from '../shared/Component.js';
+
+class SpellApp extends Component {
+  renderTemplate() {
+    return /*html*/`
+        <div>
+          <main>
+          </main>
+        </div>
+    `;
+  }
+}
+export default SpellApp;
