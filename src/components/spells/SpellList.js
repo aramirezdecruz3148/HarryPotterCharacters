66f31d5ee98158a1f1cc02bc2dcb65cd851c2c40
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-14 19:18:23 -0700

    trying to get around the apis limitations and find a way to make the spells searchable

--- src/components/spells/SpellList.js
+++ src/components/spells/SpellList.js
@@ -5,10 +5,12 @@ class SpellList extends Component {
     render() {
         const list = this.renderDOM();
         const spells = this.props.spells;
+
         spells.forEach(spell => {
             const spellItem = new SpellItem({ spell });
             list.appendChild(spellItem.render());
-        });
+         });
+        
 
         return list;
     }
