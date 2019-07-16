92349a2459d0cf04feb7e5e43a4c1fffea7fbd49
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-16 08:43:08 -0700

    added a title animation

--- src/components/home/HomeHeader.js
+++ src/components/home/HomeHeader.js
@@ -1,10 +1,21 @@
 import Component from '../shared/Component.js';
 
 class HomeHeader extends Component {
+    render() {
+        const dom = this.renderDOM();
+        const title = dom.querySelector('#title-pic')
+
+        onload = function animateText() {
+            title.classList.add('animate');
+        };
+
+        return dom;
+    }
+
     renderTemplate() {
         return /*html*/`
             <header>
-                <img src="./assets/title.png" alt="hp logo">
+                <img id="title-pic" src="./assets/title.png" alt="hp logo">
             </header>
         `;
     }
