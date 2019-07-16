69f2f83f0cae76311738d3c269f65e4f9ab0c622
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-16 08:30:45 -0700

    tried to add music, not sure that it is working

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,6 +1,8 @@
 import HomeApp from './HomeApp.js';
 
 const homeApp = new HomeApp();
+const audio = document.getElementById('audio');
+audio.src = '../../assets/hp-theme.m4a';
 
 const root = document.getElementById('app');
 root.appendChild(homeApp.render());
