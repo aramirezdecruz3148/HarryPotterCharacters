767ae6148c70699b5fde763cf9220de8635d0c5a
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-16 08:16:22 -0700

    added more styling to each directory card with links to the pages

--- src/components/home/DirectoryLinks.js
+++ src/components/home/DirectoryLinks.js
@@ -5,21 +5,21 @@ class DirectoryLinks extends Component {
         return /*html*/`
             <ul class="directory">
                 <li class="character-directory">
-                    <h3>Character Directory<span class="feet">👣</span></h3>
+                    <h3 class="title">Character Directory<span class="feet">👣</span></h3>
                     <section class="hidden">
                         <hr>
-                        <p class="card-text">Placeholder</p>
-                        <p class="card-text">Placeholder</p>
-                        <p class="card-text">Placeholder</p>
+                        <p class="card-text">A list of all characters from the Harry Potter
+                        books. Filter through the list by house, blood status, or group affiliation.</p>
+                        <button id="characters-directory"><a href="/characters.html">Directory of Characters</a></button>
                     </section>
                 </li> 
                 <li class="spell-directory">
-                    <h3>Spell Directory<span class="feet">✨</span></h3>
+                    <h3 class="title">Spell Directory<span class="feet">✨</span></h3>
                     <section class="hidden">
                         <hr>
-                        <p class="card-text">Placeholder</p>
-                        <p class="card-text">Placeholder</p>
-                        <p class="card-text">Placeholder</p>
+                        <p class="card-text">A list of all spells from the Harry Potter
+                        books in alphabetical order.</p>
+                        <button id="spells-directory"><a href="/spells.html">Directory of Spells</a></button>
                     </section>
                 </li> 
             </ul>
